8000c67f6d9998f839f022ff9625529e5ce4a984
173799761 <173799761@users.noreply.github.com>
2023-04-14 19:50:57 +0800

    1
    1

--- course4/erc20Ext/truffle-config.js
+++ course4/erc20Ext/truffle-config.js
@@ -41,10 +41,16 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require('dotenv').config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const privateKey = process.env.privateKey;
+const infuraId = process.env.infuraId;
+
+console.log(privateKey)
+console.log(infuraId)
 
 module.exports = {
   /**
@@ -82,13 +88,15 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(privateKey, `https://goerli.infura.io/v3/${infuraId}`),
+      network_id: 5,       // Goerli's id     
+
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      networkCheckTimeout: 10000,
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false, // Skip dry run before migrations? (default: false for public nets )             
+    },
     //
     // Useful for private networks
     // private: {
@@ -96,6 +104,7 @@ module.exports = {
     //   network_id: 2111,   // This network is yours, in the cloud.
     //   production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    
   },
 
   // Set default mocha options here, use special reporters, etc.
